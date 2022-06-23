589876a183a46c3cb2aca5164e1ade1665877e8e
Kingsley <oparaudochukwu277@gmail.com>
2022-06-23 03:49:34 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const personality = {
+    Name: 'Opara Udochukwu',
+    Height: '5.6ft',
+    Country: 'Nigeria'
+};
+const {Name, Height, Country} = personality
+const result = `I'm ${Name}, ${Height} tall in height and from ${Country}`
+console.log(result)
+document.write(result)
